e8c3eb3cddfb985498881ce25a3bf3be6452f892
danvasquez <dan@maestroinfo.com>
2013-05-17 16:05:13 -0400

    adds hello.php

---
+++ php/hello.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: danvasquez
+ * Date: 5/15/13
+ * Time: 10:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo phpinfo();
+
+?>
